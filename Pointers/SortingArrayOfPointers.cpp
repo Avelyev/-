f1f7674147f2a16f241e0f6c145143de8224ed6b
Eugene Bukowski <eugene@codrob.ru>
2016-09-08 13:42:42 +0300

    Error in SortingArrayOfPointers.cpp

--- Pointers/SortingArrayOfPointers.cpp
+++ Pointers/SortingArrayOfPointers.cpp
@@ -31,26 +31,34 @@ class Person {
 public:
 	int data;
 	int data2;
+	int data3;
+	int data4;
 
-	Person()
-	{
-		data = 0;
-		data2 = 0;
-	}
+	Person ()
+	{}
+
+	Person(int data, int data2, int data3, int data4) : data(data), data2(data2), data3(data3), data4(data4)
+	{}
 };
 
 inline void sortPointer (Person *p1, Person *p2)
 {
 	Person *temp = p1;
+	//Элемент массива под индексом p1 теперь хранит другой адрес, из p2
 	*p1 = *p2;
 	*p2 = *temp;
 }
 
 inline void sortObject (Person *p1, Person *p2)
 {
-	Person *temp = p1;
-	p1 = p2;
-	p2 = temp;
+	//Извлекаем (разыменовываем) объект Person
+	Person temp = *p1;
+
+	//ИСПРАВИТЬ
+	p1->data = p2->data;
+	p1->data2 = p2->data2;
+	p2->data = temp->data;
+	p2->data2 = temp->data2;
 }
 
 void sort(Person **person, int n, void (*f)(Person*, Person*))
@@ -61,6 +69,7 @@ void sort(Person **person, int n, void (*f)(Person*, Person*))
 		{
 			if (person[i]->data > person[j]->data)
 			{
+				//Передаем 2 указателя на ячейки памяти в которых хранятся объекты типа Person
 				f(person[i], person[j]);
 			}
 		}
@@ -72,23 +81,24 @@ int main()
 	void (*sortPointerPtr) (Person*, Person*) = &sortPointer;
 	void (*sortObjectPtr) (Person*, Person*) = &sortObject;
 
-	const int n = 30000;
+	const int n = 20000;
 
 	//Array pointers of objects
-	Person *person[n];
+	Person* person[n];
 
 	//Creating objects and add pointers in array
 	for (int i = 0; i < n; i++)
 	{
-		person[i] = new Person();
-		person[i]->data = i;
-		person[i]->data2 = i;
+		person[i] = new Person(i, i, i, i);
 	}
+
+	//Хранит адрес объекта Person
+	cout << person[10] << endl;
 
 	cout << "Run sort. Last element: " << person[n - 1]->data << " | " << person[n - 1]->data2 << endl;
 
-	sort(person, n, sortPointerPtr);
-	//sort(person, n, sortObjectPtr);
+	//sort(person, n, sortPointerPtr);
+	sort(person, n, sortObjectPtr);
 
 	cout << "End sort. Last element: " << person[n - 1]->data << " | " << person[n - 1]->data2 << endl;
 
